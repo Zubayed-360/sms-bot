7c712d22a27ea2773abd65fd55a1e72114aa0551
Zubayed islam <110227053+Zubayed-360@users.noreply.github.com>
2026-03-29 05:04:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,35 +8,42 @@ BOT_TOKEN = "8682824157:AAHbEe9794qQnpNKSVqirherACuIcqLCzdc"
 
 BASE_URL = "https://smsbower.com/stubs/handler_api.php"
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("⏳ Loading country & price list...")
 
-    url = f"{BASE_URL}?api_key={API_KEY}&action=getPrices&service=tgff"
+    url = f"{BASE_URL}?api_key={API_KEY}&action=getPrices&service=tgfr"
     res = requests.get(url).text
 
     text = "🌍 Country + Price List:\n\n"
     text += res[:3500]
-    text += "\n\n📌 Reply with country ID (example: 22)"
+    text += "\n\n📩 Reply with country ID (example: 22)"
 
     await update.message.reply_text(text)
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+
     cid = update.message.text.strip()
 
-    await update.message.reply_text(f"✅ Country selected: {cid}\n📲 Getting number...")
+    await update.message.reply_text(f"✅ Country selected: {cid}\n⏳ Getting number...")
 
-    url = f"{BASE_URL}?api_key={API_KEY}&action=getNumber&service=tgff&country={cid}"
+    url = f"{BASE_URL}?api_key={API_KEY}&action=getNumber&service=tgfr&country={cid}"
+
     res = requests.get(url).text
 
     if "ACCESS_NUMBER" in res:
+
         data = res.split(":")
+
         activation_id = data[1]
+
         number = data[2]
 
-        await update.message.reply_text(f"📞 Number: {number}\n⏳ Waiting SMS...")
+        await update.message.reply_text(f"📱 Number: {number}\n⏳ Waiting SMS...")
 
-        for _ in range(20):
+        for i in range(20):
+
             await asyncio.sleep(5)
 
             status = requests.get(
@@ -44,19 +51,26 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             ).text
 
             if "STATUS_OK" in status:
+
                 code = status.split(":")[1]
+
                 await update.message.reply_text(f"✅ SMS Code: {code}")
+
                 return
 
         await update.message.reply_text("❌ SMS not received")
+
     else:
+
         await update.message.reply_text("❌ No number available")
 
 
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
+
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
 print("Bot running...")
+
 app.run_polling()
